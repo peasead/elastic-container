d28e467ec0b418ea73f803d98cb19673a11eaabb
variable <7442091+peasead@users.noreply.github.com>
2021-03-28 22:23:17 -0500

    filez

---
+++ elastic-container.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Usage: 
+# "./elastic-container.sh start" to start an Elasticsearch node and connected Kibana instance.
+# "./elastic-container.sh stop" to stop an Elasticsearch node and connected Kibana instance.
+# "./elastic-container.sh status" to get the status of the Elasticsearch node and connected Kibana instance.
+# No data is retained!
+
+if [ $1 == start ]
+then
+docker network create elk
+docker run -d --network elk --rm --name elasticsearch -p 9200:9200 -p 9300:9300 \
+-e "discovery.type=single-node" \
+-e "xpack.security.enabled=true" \
+-e "xpack.security.authc.api_key.enabled=true" \
+-e "ELASTIC_PASSWORD=password" \
+docker.elastic.co/elasticsearch/elasticsearch:8.0.0-SNAPSHOT
+docker run -d --network elk --rm --name kibana -p 5601:5601 \
+-v $(pwd)/kibana.yml:/usr/share/kibana/config/kibana.yml \
+-e "ELASTICSEARCH_HOSTS=http://elasticsearch:9200" \
+-e "ELASTICSEARCH_USERNAME=elastic" \
+-e "ELASTICSEARCH_PASSWORD=password" \
+docker.elastic.co/kibana/kibana:8.0.0-SNAPSHOT
+
+else
+if [ $1 == stop ]
+then
+docker stop elasticsearch
+docker stop kibana
+docker network rm elk
+
+else
+if [ $1 == status ]
+then
+docker ps -f "name=kibana" -f "name=elasticsearch" --format "table {{.Names}}: {{.Status}}"
+
+else
+echo "Proper syntax not used. Try ./elastic-docker {start,stop,status}"
+fi
+fi
+fi
