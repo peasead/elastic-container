c6b360709f2c1f06ed32b216f71b1975d9efdc5c
Andrew Pease <7442091+peasead@users.noreply.github.com>
2021-05-25 14:32:38 -0500

    added more commenting

--- elastic-container.sh
+++ elastic-container.sh
@@ -20,28 +20,39 @@ ELASTICSEARCH_URL="http://elasticsearch:9200"
 STACK_VERSION="7.13.0"
 #STACK_VERSION="8.0.0-SNAPSHOT"
 
+# Collect the Elastic and Kibana Docker images
 if [ $1 == stage ] 2> /dev/null
 then
 docker pull docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
 docker pull docker.elastic.co/kibana/kibana:${STACK_VERSION}
 
+# Create the Docker network
 else
 if [ $1 == start ] 2> /dev/null
 then
 docker network create elastic 2> /dev/null
+
+# Start the Elasticsearch container
 docker run -d --network elastic --rm --name elasticsearch -p 9200:9200 -p 9300:9300 \
 -e "discovery.type=single-node" \
 -e "xpack.security.enabled=true" \
 -e "xpack.security.authc.api_key.enabled=true" \
 -e "ELASTIC_PASSWORD=${ELASTIC_PASSWORD}" \
 docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION}
+
+# Start the Kibana container
 docker run -d --network elastic --rm --name kibana -p 5601:5601 \
 -v $(pwd)/kibana.yml:/usr/share/kibana/config/kibana.yml \
 -e "ELASTICSEARCH_HOSTS=${ELASTICSEARCH_URL}" \
 -e "ELASTICSEARCH_USERNAME=elastic" \
 -e "ELASTICSEARCH_PASSWORD=${ELASTIC_PASSWORD}" \
 docker.elastic.co/kibana/kibana:${STACK_VERSION}
+echo
+echo "Browse to http://localhost:5601"
+echo "Username: elastic"
+echo "Passphrase: ${ELASTIC_PASSWORD}"
 
+# Stop and remove the Elastic containers and network
 else
 if [ $1 == stop ] 2> /dev/null
 then
@@ -49,17 +60,20 @@ docker stop elasticsearch 2> /dev/null
 docker stop kibana 2> /dev/null
 docker network rm elastic 2> /dev/null
 
+# Restart the Elastic containers
 else
 if [ $1 == restart ] 2> /dev/null
 then
 docker restart elasticsearch 2> /dev/null
 docker restart kibana 2> /dev/null
 
+# Get the status of the Elastic containers
 else
 if [ $1 == status ] 2> /dev/null
 then
 docker ps -f "name=kibana" -f "name=elasticsearch" --format "table {{.Names}}: {{.Status}}"
 
+# Usage helper
 else
 echo "Proper syntax not used. Try ./elastic-container {stage,start,stop,restart,status}"
 fi
