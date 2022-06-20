1dfdc12e5ff5565626e7d09b505f71df274281b5
DefSecSentinel <defsecsentinel@protonmail.ch>
2022-06-20 09:49:20 -0500

    Update Fleet settings

--- elastic-container.sh
+++ elastic-container.sh
@@ -82,7 +82,7 @@ set_fleet_values() {
   printf '{"fleet_server_hosts": ["%s"]}' "https://${ipvar}:${FLEET_PORT}" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/settings" -d @- | jq 
   printf '{"hosts": ["%s"]}' "https://${ipvar}:9200" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq 
   printf '{"ca_trusted_fingerprint": "%s"}' "${fingerprint}" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq 
-  printf '{"config_yaml": "%s"}' "ssl.verification.mode: certificate" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq 
+  printf '{"config_yaml": "%s"}' "ssl.verification.mode: none" | curl -k --silent --user "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -XPUT "${HEADERS[@]}" "${LOCAL_KBN_URL}/api/fleet/outputs/fleet-default-output" -d @- | jq 
 }
 
 # Logic to enable the verbose output if needed
@@ -139,6 +139,7 @@ case "${ACTION}" in
   echo
 
   echo "READY SET GO!"
+  echo
   echo "Browse to https://localhost:5601"
   echo "Username: ${ELASTIC_USERNAME}"
   echo "Passphrase: ${ELASTIC_PASSWORD}"
